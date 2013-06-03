a8e6a7f96e06d7e1df93cfcda5e72cc57eec1c37
m1LLo <m.leitz@freenet.de>
2013-06-03 17:01:45 +0200

    Hinzufügen AutoTest

---
+++ Uebung6/AutoTest.cpp
@@ -0,0 +1,18 @@
+/**
+ * TODO
+ *	
+ * @file:		AutoTest.cpp
+ * @date: 		03.06.2013
+ * @author:	 	Markus Leitz (3543994)
+ */
+
+#include "AutoTest.h"
+
+AutoTest::AutoTest()
+{
+
+}
+
+AutoTest::~AutoTest()
+{
+}
---
+++ Uebung6/AutoTest.h
@@ -0,0 +1,26 @@
+/**
+ * TODO
+ *	
+ * @file:		AutoTest.h
+ * @date: 		03.06.2013
+ * @author:	 	Markus Leitz (3543994)
+ */
+
+#ifndef AUTOTEST_H_
+#define AUTOTEST_H_
+
+#include "LinList.h"
+
+/**
+ * @author Markus Leitz (3543994)
+ * @version
+ * @date 03.06.2013
+ */
+class AutoTest
+{
+private:
+	AutoTest();
+	~AutoTest();
+};
+
+#endif /* AUTOTEST_H_ */
